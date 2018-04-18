8d42407f8b33486ae6d6aa39bd0f0ee6e3536134
Lrssn <lrssn95@gmail.com>
2018-04-18 18:34:49 +0200

    system pause instead of pause()

--- Lua_to_C/Main.cpp
+++ Lua_to_C/Main.cpp
@@ -1,16 +1,14 @@
 #include <lua.hpp>
 #include <iostream>
-void pause() {
-	char pause;
-	std::cout << "Press any key to continue...";
-	std::cin >> pause;
-}
+
 int main() {
 	lua_State *L = luaL_newstate();
 	luaL_openlibs(L);
+	
 	luaL_dofile(L, "Test.lua");
+	
 	lua_close(L);
-	pause();
+	system("pause");
 	return 0;
 }
 
